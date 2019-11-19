3578320b6a3a8e177882a6e029186d667b59ab1e
kdnakt <a.kid.1985@gmail.com>
2019-11-20 08:06:29 +0900

    add executionRoleArn

--- service/aws_ecs_task_definition.tf
+++ service/aws_ecs_task_definition.tf
@@ -2,6 +2,7 @@ resource "aws_ecs_task_definition" "sample-task" {
   family = "webapp-service"
   container_definitions = file("./container_definitions/service.json")
   task_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  execution_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   network_mode = "bridge"
 }
 
@@ -9,5 +10,6 @@ resource "aws_ecs_task_definition" "sample-migration-task" {
   family = "webapp-service"
   container_definitions = file("./container_definitions/migration.json")
   task_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  execution_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   network_mode = "bridge"
 }
