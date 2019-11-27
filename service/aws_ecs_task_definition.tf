5c7d71d4a8f24cde2a20bb8db5bb1f8595b08747
kdnakt <a.kid.1985@gmail.com>
2019-11-28 02:08:42 +0900

    fix family name for task def

--- service/aws_ecs_task_definition.tf
+++ service/aws_ecs_task_definition.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "sample-task" {
 }
 
 resource "aws_ecs_task_definition" "sample-migration-task" {
-  family = "webapp-service"
+  family = "webapp-migration"
   container_definitions = file("./container_definitions/migration.json")
   task_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   execution_role_arn = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
